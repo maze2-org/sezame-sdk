2ce97f5bfc44a06e4c1b86221ab2327996311d57
Muhammed <aapozaid@gmail.com>
2022-02-11 05:15:24 +0200

    adding AvnGenerator public address from mnemonic

---
+++ src/generators/drivers/avn.ts
@@ -0,0 +1,43 @@
+import { CONFIG } from '../../utils/config';
+import { GenericGenerator } from '../GenericGenerator';
+
+import { Keyring } from '@polkadot/keyring';
+import { u8aToHex } from '@polkadot/util';
+import {
+  cryptoWaitReady,
+  mnemonicGenerate,
+  mnemonicToMiniSecret,
+} from '@polkadot/util-crypto';
+
+/**
+ *
+ *
+ * @export
+ * @class AVNGenerator
+ * @extends {GenericGenerator}
+ */
+export class AvnGenerator extends GenericGenerator {
+  static async generateWalletXpub(
+    mnemonic: any,
+    accountCrypto: any,
+    config: any = CONFIG
+  ) {
+    let crypto = accountCrypto || 'sr25519';
+
+    if (!['ed25519', 'sr25519'].includes(crypto)) {
+      throw `Invalid account crypto "${crypto}" specified.`;
+    }
+
+    await cryptoWaitReady();
+
+    const keyring = new Keyring({ type: crypto, ss58Format: 42 });
+
+    const keyPair = keyring.createFromUri(mnemonic);
+    return u8aToHex(keyPair.publicKey);
+  }
+  static async generatePrivateKeyFromMnemonic(mnemonic: any) {
+    await cryptoWaitReady();
+
+    return u8aToHex(mnemonicToMiniSecret(mnemonic));
+  }
+}
--- src/generators/index.ts
+++ src/generators/index.ts
@@ -2,3 +2,4 @@ export { BitcoinGenerator } from './drivers/bitcoin';
 export { BscGenerator } from './drivers/bsc';
 export { EthereumGenerator } from './drivers/ethereum';
 export { PolygonGenerator } from './drivers/polygon';
+export { AvnGenerator } from './drivers/avn';
