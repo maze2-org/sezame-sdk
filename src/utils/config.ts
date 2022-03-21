dca4a225ce5d32e697f75c5f2859841e6455d0cb
Muhammed <aapozaid@gmail.com>
2022-03-21 05:10:55 +0200

    make TESTNET dynamically configurable

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -25,6 +25,7 @@ export interface IConfig {
   DEFAULT_DERIVATION_KEY: number;
   CHAIN_ENDPOINTS: CHAIN_ENDPOINTS_MAP;
   SERVICES_ENDPOINTS: SERVICES_ENDPOINTS_MAP;
+  setTESTNET?(testnet: boolean): void;
 }
 
 const TESTNET = String(process.env.TESTNET) === 'true';
@@ -32,4 +33,29 @@ const TESTNET = String(process.env.TESTNET) === 'true';
 const MAINNET_CONFIG: IConfig = MAINNET_CONFIG_DATA;
 const TESTNET_CONFIG: IConfig = TESTNET_CONFIG_DATA;
 
-export const CONFIG: IConfig = TESTNET ? TESTNET_CONFIG : MAINNET_CONFIG;
+export class Config implements IConfig {
+  public TESTNET: boolean;
+  public DEFAULT_DERIVATION_KEY!: number;
+  public CHAIN_ENDPOINTS!: CHAIN_ENDPOINTS_MAP;
+  public SERVICES_ENDPOINTS!: SERVICES_ENDPOINTS_MAP;
+
+  constructor(testNet: boolean) {
+    this.TESTNET = testNet;
+    this.setConfig();
+  }
+
+  public setConfig() {
+    const config: IConfig = this.TESTNET ? TESTNET_CONFIG : MAINNET_CONFIG;
+    this.TESTNET = config.TESTNET;
+    this.DEFAULT_DERIVATION_KEY = config.DEFAULT_DERIVATION_KEY;
+    this.CHAIN_ENDPOINTS = config.CHAIN_ENDPOINTS;
+    this.SERVICES_ENDPOINTS = config.SERVICES_ENDPOINTS;
+  }
+
+  public setTESTNET(value: boolean) {
+    this.TESTNET = value;
+    this.setConfig();
+  }
+}
+
+export const CONFIG: IConfig = new Config(TESTNET);
